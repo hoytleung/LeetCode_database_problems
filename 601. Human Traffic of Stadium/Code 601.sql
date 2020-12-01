584081b6a3f0f5f2b2e3590e27a7aad4613a289a
hoytleung <61213323+hoytleung@users.noreply.github.com>
2020-12-01 07:43:00 +0100

    Update Code 601.sql

--- 601. Human Traffic of Stadium/Code 601.sql
+++ 601. Human Traffic of Stadium/Code 601.sql
@@ -1,16 +1,16 @@
-With Consecutive AS (   
+With Consecutive_id AS (   
     SELECT S1.id AS S1_id, S1.visit_date S1_visitdate, S1.people AS S1_people, S2.id AS S2_id, S2.visit_date S2_visitdate, S2.people AS S2_people, S3.id AS S3_id, S3.visit_date S3_visitdate, S3.people AS S3_people
     FROM Stadium AS S1, Stadium AS S2, Stadium AS S3
     WHERE S2.id = S1.id +1 AND S3.id = S1.id +2 AND S1.people >=100 AND S2.people >=100 AND S3.people >=100),
         
      S1 AS (
-    SELECT S1_id AS id, S1_visitdate AS visit_date, S1_people AS people FROM Consecutive),
+    SELECT S1_id AS id, S1_visitdate AS visit_date, S1_people AS people FROM Consecutive_id),
     
      S2 AS (
-    SELECT S2_id AS id, S2_visitdate AS visit_date, S2_people AS people FROM Consecutive),
+    SELECT S2_id AS id, S2_visitdate AS visit_date, S2_people AS people FROM Consecutive_id),
     
      S3 AS (            
-    SELECT S3_id AS id, S3_visitdate AS visit_date, S3_people AS people FROM Consecutive)
+    SELECT S3_id AS id, S3_visitdate AS visit_date, S3_people AS people FROM Consecutive_id)
     
     
 
